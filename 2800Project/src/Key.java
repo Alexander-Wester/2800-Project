18ea14ad89dc04af59cbb093df4a789e082543ca
colem <colemorgan152@gmail.com>
2024-04-06 18:53:31 -0400

    Added keys to level 10 of the left and right side to unlock the boss door

---
+++ 2800Project/src/Key.java
@@ -0,0 +1,36 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Key extends Enemy{
+    private boolean collected = false;
+    private BufferedImage image;
+    public Key(int x, int y) {
+        super(x, y, 20, 30, 0);
+        hitBox = new Rectangle(x,y,20,30);
+        loadImage();
+        isAlive = false;
+        isInvincible = true;
+    }
+
+    public void tick(GameManager gm){
+        if(hitBox.intersects(gm.player.rect()) && !collected){
+            collected = true;
+            gm.player.collectKey();
+        }
+    }
+    public void render(Graphics2D g2d){
+        if(!collected) {
+            g2d.drawImage(image,(int)x-40,(int)y-40,100,100,null);
+        }
+    }
+    public void loadImage(){
+        try{
+            image = ImageIO.read(new File("2800Project/lib/key.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
